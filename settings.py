641bf188091e13845278bc80c4460b806166bc53
vvhornung <vvhornung@eafit.edu.co>
2025-03-27 12:12:04 -0500

    setting hosts for vercel.app

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4#5&$!@#%&*()_+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*/vercel.app']
 
 
 # Application definition
